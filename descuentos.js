42a2b1297d97ba1b5991d701f3482777e3d84b80
Andres Gordon <gordon_andres@yahoo.es>
2022-04-14 18:29:28 -0500

    Clase 2: Implementacion de info para usuario en HTML

--- descuentos.js
+++ descuentos.js
@@ -1,4 +1,4 @@
-const precioOriginal = 120;
+/* const precioOriginal = 120;
 const descuento = 18; // 20%
 
 const porcentajePrecioOriginalConDescuento = 100 - descuento;
@@ -11,11 +11,29 @@ console.log({
   descuento,
   porcentajePrecioOriginalConDescuento,
   precioConDescuento,
-});
+}); */
 
 function calcularPrecioConDescuento(precio, descuento) {
   const porcentajePrecioConDescuento = 100 - descuento;
   const precioConDescuento = (precio * porcentajePrecioConDescuento) / 100;
 
   return precioConDescuento;
+}
+
+// Interaccion con HTML
+
+function onClickButtonPriceDiscount() {
+  const input_price = document.getElementById("InputPrice");
+  const input_discount = document.getElementById("InputPrice");
+
+  const price_value = input_price.value;
+  const discount_value = input_discount.value;
+
+  const precio_con_descuento = calcularPrecioConDescuento(
+    price_value,
+    discount_value
+  );
+
+  const resultPrice = document.getElementById("ResultPrice");
+  resultPrice.innerHTML = `El precio con descuento es: $ ${precio_con_descuento}`;
 }
