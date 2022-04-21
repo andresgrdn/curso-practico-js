f4f2cdbd46729a45818178309b3f8fa8f9ad9952
Andres Gordon <gordon_andres@yahoo.es>
2022-04-21 18:54:03 -0500

    Reto terminado

---
+++ pobreza.js
@@ -0,0 +1,45 @@
+// Helpers
+
+function estaEnElUmbral(salario, personasEnDependencia) {
+  const umbralPobreza = 84.71;
+  const umbralPobrezaExtrema = 47.74;
+
+  const ingresoFamiliarPerCapita = salario / personasEnDependencia;
+
+  // Analisis de pobreza
+  let msg;
+
+  if (ingresoFamiliarPerCapita < umbralPobrezaExtrema) {
+    msg = `Su ingreso familiar por persona es $ ${ingresoFamiliarPerCapita} es menor al umbral de pobreza extrema en Ecuador de $ ${umbralPobrezaExtrema}`;
+  } else if (ingresoFamiliarPerCapita < umbralPobreza) {
+    msg = `Su ingreso familiar por persona es $ ${ingresoFamiliarPerCapita} es menor al umbral de pobreza en Ecuador de $ ${umbralPobreza}`;
+  } else {
+    msg = `Felicidades se encuentra fuera del umbral de pobreza`;
+  }
+
+  return msg;
+}
+
+// Comparacion con el SBU
+const salarioBasicoUnificado = 425;
+const salario = 100;
+
+const gapSalarioBasicoUnificado = salario - salarioBasicoUnificado;
+
+console.log(`La diferencia con el SBU es: ${gapSalarioBasicoUnificado}`);
+
+// Interaccion con el HTML
+function verificarUmbral() {
+  const input_salario = document.getElementById("InputSalario");
+  const input_personasEnDependencia = document.getElementById(
+    "InputPersonasEnDependencia"
+  );
+
+  const sueldo = input_salario.value;
+  const personasEnDependencia = input_personasEnDependencia.value;
+
+  const msg = estaEnElUmbral(sueldo, personasEnDependencia);
+
+  const elem_resultado = document.getElementById("Resultado");
+  elem_resultado.innerText = msg;
+}
